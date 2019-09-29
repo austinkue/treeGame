3a6109a9fb97e3634d9179fbd1d797b802dc9bbb
Loi Huynh <loihuynh@oakland.edu>
2019-09-28 23:41:43 -0400

    3buttons
    3 buttons, text and acorn panels

---
+++ Tree Game/Assets/Scripts/AcornPanelAction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AcornPanelAction : MonoBehaviour
+{
+
+    public GameObject roots2,AcornPanelHead,AcornPanelLeft, rodentKO, Button1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AcornPanelHead.SetActive(false);
+        AcornPanelLeft.SetActive(false);
+        rodentKO.SetActive(false);
+        roots2.SetActive(false);
+    }
+
+    public void LightOnFire()
+    {
+        AcornPanelHead.SetActive(true);
+        
+        Time.timeScale = 0;
+    }
+    
+    public void AlexJones()
+    {
+        AcornPanelLeft.SetActive(true);
+        rodentKO.SetActive(true);
+        Time.timeScale = 1;
+    }
+    public void roots()
+    {
+        roots2.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
